f4b9f33082549f14ff7e97f7a53afc8aba48ccb8
Hua Trung <huatrung@Huas-MacBook-Air.local>
2022-12-31 18:33:32 +0700

    add

---
+++ client/build/precache-manifest.3c12c51a95a00ee01a872b7013a0c731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f3a393fb93e199c9b51ddf63435fa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "759accbed22b28087355",
+    "url": "/static/css/main.b2d9a5cc.chunk.css"
+  },
+  {
+    "revision": "4e978dd403b36a848888",
+    "url": "/static/js/2.28fb2d21.chunk.js"
+  },
+  {
+    "revision": "759accbed22b28087355",
+    "url": "/static/js/main.288eba73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
